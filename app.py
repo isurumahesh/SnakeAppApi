64d33d2375f7d301a230dc4a8d363b2020bb1a08
isuruw <isuruw@99x.lk>
2020-05-06 10:20:57 +0530

    add new model

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ CORS(app)
 
 def get_model():
     global model
-    model=load_model("snake_rgb_model_x_3_activation.h5")
+    model=load_model("snake_mobilenet_10.h5")
 
 
 get_model()
@@ -36,17 +36,17 @@ def predict():
     try:
         message=request.get_json(force=True)  
         encoded=message["name"]
-        print(encoded)
+       
         decoded=base64.b64decode(encoded)
         image=Image.open(io.BytesIO(decoded))
-        processedImage=preprocessImage(image,target_size=(50,50))
+        processedImage=preprocessImage(image,target_size=(96,96))
         X=processedImage/255 
-        print(X)
+       
         result=model.predict(X)
-        number_to_class=["class-204","class-697","class-872"]
+        number_to_class=["class-78","class-204","class-508","class-543","class-581","class-697","class-771","class-804","class-872","class-966"]
         index=np.argsort(result[0,:])
-        result='Most likely class:'+str(number_to_class[index[2]])+'--probability:'+str(result[0,index[2]])
-        print(result)
+        result='Most likely class:'+str(number_to_class[index[9]])+'--probability:'+str(result[0,index[9]])
+       
         person = {'result':result}
         return jsonify(person)
     except Exception as e:
